0ec6953378bbddf135a822ea4ce484045c8f4486
kolpakovee <eekolpakov@edu.hse.ru>
2023-11-02 12:36:33 +0300

    add cart

---
+++ app.js
@@ -0,0 +1,77 @@
+const itemCountElement = document.querySelector('.item-count');
+const addButton1 = document.getElementById('btn1');
+const addButton2 = document.getElementById('btn2');
+const addButton3 = document.getElementById('btn3');
+const addButton4 = document.getElementById('btn4');
+const addButton5 = document.getElementById('btn5');
+const addButton6 = document.getElementById('btn6');
+const addButton7 = document.getElementById('btn7');
+const addButton8 = document.getElementById('btn8');
+const addButton9 = document.getElementById('btn9');
+
+// Инициализируем счетчик товаров
+let itemCount = 0;
+
+// Обработчик события для кнопки "Add"
+addButton1.addEventListener('click', function() {
+    // Увеличиваем счетчик товаров
+    itemCount++;
+    // Обновляем текст счетчика товаров
+    itemCountElement.textContent = itemCount;
+});
+
+addButton2.addEventListener('click', function() {
+    // Увеличиваем счетчик товаров
+    itemCount++;
+    // Обновляем текст счетчика товаров
+    itemCountElement.textContent = itemCount;
+});
+
+addButton3.addEventListener('click', function() {
+    // Увеличиваем счетчик товаров
+    itemCount++;
+    // Обновляем текст счетчика товаров
+    itemCountElement.textContent = itemCount;
+});
+
+addButton4.addEventListener('click', function() {
+    // Увеличиваем счетчик товаров
+    itemCount++;
+    // Обновляем текст счетчика товаров
+    itemCountElement.textContent = itemCount;
+});
+
+addButton5.addEventListener('click', function() {
+    // Увеличиваем счетчик товаров
+    itemCount++;
+    // Обновляем текст счетчика товаров
+    itemCountElement.textContent = itemCount;
+});
+
+addButton6.addEventListener('click', function() {
+    // Увеличиваем счетчик товаров
+    itemCount++;
+    // Обновляем текст счетчика товаров
+    itemCountElement.textContent = itemCount;
+});
+
+addButton7.addEventListener('click', function() {
+    // Увеличиваем счетчик товаров
+    itemCount++;
+    // Обновляем текст счетчика товаров
+    itemCountElement.textContent = itemCount;
+});
+
+addButton8.addEventListener('click', function() {
+    // Увеличиваем счетчик товаров
+    itemCount++;
+    // Обновляем текст счетчика товаров
+    itemCountElement.textContent = itemCount;
+});
+
+addButton9.addEventListener('click', function() {
+    // Увеличиваем счетчик товаров
+    itemCount++;
+    // Обновляем текст счетчика товаров
+    itemCountElement.textContent = itemCount;
+});
